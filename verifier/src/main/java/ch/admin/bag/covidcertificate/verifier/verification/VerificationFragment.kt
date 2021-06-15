f9aa7635cb02a548e24db9aef30005b46fb2a424
Mike Wong <wong@ubique.ch>
2021-06-15 15:28:39 +0200

    Reset verifier to home screen when put into background

--- verifier/src/main/java/ch/admin/bag/covidcertificate/verifier/verification/VerificationFragment.kt
+++ verifier/src/main/java/ch/admin/bag/covidcertificate/verifier/verification/VerificationFragment.kt
@@ -18,10 +18,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.core.view.doOnLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import ch.admin.bag.covidcertificate.common.util.getHeaderColor
 import ch.admin.bag.covidcertificate.common.util.getInfoIconColor
@@ -59,6 +61,7 @@ class VerificationFragment : Fragment() {
 	private val binding get() = _binding!!
 	private val verificationViewModel: VerificationViewModel by viewModels()
 	private var dccHolder: DccHolder? = null
+	private var isClosedByUser = false
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -67,6 +70,13 @@ class VerificationFragment : Fragment() {
 		}
 
 		dccHolder = requireArguments().getSerializable(ARG_DECODE_DGC) as DccHolder
+
+		requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+			override fun handleOnBackPressed() {
+				isClosedByUser = true
+				parentFragmentManager.popBackStack()
+			}
+		})
 	}
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -87,7 +97,10 @@ class VerificationFragment : Fragment() {
 		binding.verificationStandardizedNameLabel.text =
 			"${eudgc.person.standardizedFamilyName}<<${eudgc.person.standardizedGivenName}"
 
-		binding.verificationFooterButton.setOnClickListener { parentFragmentManager.popBackStack() }
+		binding.verificationFooterButton.setOnClickListener {
+			isClosedByUser = true
+			parentFragmentManager.popBackStack()
+		}
 
 		view.doOnLayout { setupScrollBehavior() }
 
@@ -96,6 +109,15 @@ class VerificationFragment : Fragment() {
 		verificationViewModel.verificationLiveData.observe(viewLifecycleOwner, {
 			updateHeaderAndVerificationView(it)
 		})
+	}
+
+	override fun onPause() {
+		super.onPause()
+		// Pop the entire backstack back to the home screen when the verification fragment is put into the background, unless
+		// it was closed by the user (e.g. with the back or OK button)
+		if (!isClosedByUser) {
+			parentFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+		}
 	}
 
 	override fun onDestroyView() {
