f757bc213c16fa34868f42f8a89a7c405b0ef092
Thore Goebel <goebel@ubique.ch>
2021-06-17 14:49:21 +0200

    Add transfer crypto functions

---
+++ wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/inappdelivery/InAppDeliveryCrypto.kt
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package ch.admin.bag.covidcertificate.wallet.inappdelivery
+
+import android.app.Application
+import android.content.pm.PackageManager
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyInfo
+import android.security.keystore.KeyProperties
+import ch.admin.bag.covidcertificate.eval.utils.fromBase64
+import ch.admin.bag.covidcertificate.eval.utils.toBase64
+import java.security.*
+import java.security.spec.RSAKeyGenParameterSpec
+import java.time.Instant
+import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+
+object InAppDeliveryCrypto {
+
+	fun createKeyPair(keyAlias: String, application: Application): KeyPair? {
+		val keyPurpose =
+			KeyProperties.PURPOSE_DECRYPT or KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+
+		val keyGenParameterSpec = KeyGenParameterSpec
+			.Builder(keyAlias, keyPurpose)
+			.apply {
+				// 2048 bit with public exponent 65537
+				setAlgorithmParameterSpec(RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4))
+				setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA1)
+				setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_OAEP)
+
+				// Encourage storing the key in strongbox (the TPM)
+				val hasStrongBox = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+					application.packageManager.hasSystemFeature(PackageManager.FEATURE_STRONGBOX_KEYSTORE)
+				} else false
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && hasStrongBox) {
+					setIsStrongBoxBacked(true)
+				}
+			}.build()
+
+		try {
+			KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore").apply {
+				initialize(keyGenParameterSpec)
+				return generateKeyPair()
+			}
+		} catch (e: Throwable) {
+			e.printStackTrace()
+			return null
+		}
+	}
+
+	fun loadKeyPair(keyAlias: String): KeyPair? {
+		val keystore = KeyStore.getInstance("AndroidKeyStore").apply {
+			load(null)
+		}
+		if (!keystore.containsAlias(keyAlias)) {
+			return null
+		}
+		val entry = keystore.getEntry(keyAlias, null) as? KeyStore.PrivateKeyEntry ?: return null
+
+		return KeyPair(entry.certificate.publicKey, entry.privateKey)
+	}
+
+	fun decryptDeliveredHealthCertificate(keyPair: KeyPair, ciphertextBase64: String): String? {
+		/*
+		 * This is somewhat like a KEM:
+		 * ciphertext = RSA-OAEP( aesIV || aesKey ) || AES-GCM(healthcert)
+		 *
+		 * Also note that all indices below are inclusive to inclusive!
+		 */
+		val ciphertext = ciphertextBase64.fromBase64()
+
+		val ciphertextWrappedAesKeyAndIv = ciphertext.sliceArray(0..255) // 256 bytes = 2048 bits => one RSA-OAEP "block"
+		val ciphertextCertificate = ciphertext.sliceArray(256..ciphertext.lastIndex)
+
+		try {
+			// Decrypt the symmetric AES that was encapsulated under RSA-OAEP
+			val rsaPlaintext = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding").run {
+				init(Cipher.DECRYPT_MODE, keyPair.private)
+				doFinal(ciphertextWrappedAesKeyAndIv)
+			}
+
+			val aesIv = rsaPlaintext.sliceArray(0..11) // 12 byte IV
+			val aesKey = rsaPlaintext.sliceArray(12..43) // 32 byte = 256 bit key
+
+			val AES_GCM_TAG_LEN = 128 // one AES block
+			val gcmSpec = GCMParameterSpec(AES_GCM_TAG_LEN, aesIv) // algorithm
+			val secretKeySpec = SecretKeySpec(aesKey, "AES") // key
+
+			// Decrypt the health certificate encrypted under AES-GCM
+			val aesPlaintext = Cipher.getInstance("AES/GCM/NoPadding").run {
+				init(Cipher.DECRYPT_MODE, secretKeySpec, gcmSpec)
+				doFinal(ciphertextCertificate)
+			}
+			return String(aesPlaintext)
+		} catch (e: Throwable) {
+			e.printStackTrace()
+			return null
+		}
+	}
+
+	fun sign(keyPair: KeyPair, message: String): String? {
+		val msg = message.encodeToByteArray()
+		try {
+			val signature = Signature.getInstance("SHA512withRSA/PSS").run {
+				initSign(keyPair.private)
+				update(msg)
+				sign()
+			}
+			return signature.toBase64()
+		} catch (e: Throwable) {
+			e.printStackTrace()
+			return null
+		}
+	}
+
+	/**
+	 * Creates a message to sign over for the delivery requsts.
+	 *
+	 * `action` should be one of: register, get, delete.
+	 * This binds the context for which the signature is intended.
+	 */
+	fun buildMessage(action: String, transferCode: String): String {
+		val timestamp = Instant.now().epochSecond
+		return "$action:$transferCode:$timestamp"
+	}
+
+}
+
+fun KeyPair.isInsideSecureHardware(): Boolean {
+	val factory: KeyFactory = KeyFactory.getInstance(private.algorithm, "AndroidKeyStore")
+	val keyInfo: KeyInfo = factory.getKeySpec(private, KeyInfo::class.java) as KeyInfo
+	return keyInfo.isInsideSecureHardware
+}
