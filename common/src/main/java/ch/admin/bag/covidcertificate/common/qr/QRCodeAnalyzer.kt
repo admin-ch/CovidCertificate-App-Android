ad7aea0e08723b789251bd6bd134c1365dd7d651
Mike Wong <wong@ubique.ch>
2021-06-23 11:34:35 +0200

    Add missing license header

--- common/src/main/java/ch/admin/bag/covidcertificate/common/qr/QRCodeAnalyzer.kt
+++ common/src/main/java/ch/admin/bag/covidcertificate/common/qr/QRCodeAnalyzer.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 package ch.admin.bag.covidcertificate.common.qr
 
 import android.graphics.ImageFormat
@@ -7,7 +17,6 @@ import ch.admin.bag.covidcertificate.eval.data.state.Error
 import com.google.zxing.*
 import com.google.zxing.common.HybridBinarizer
 import java.nio.ByteBuffer
-
 
 class QRCodeAnalyzer(
 	private val onDecodeCertificate: (decodeCertificateState: DecodeCertificateState) -> Unit,
