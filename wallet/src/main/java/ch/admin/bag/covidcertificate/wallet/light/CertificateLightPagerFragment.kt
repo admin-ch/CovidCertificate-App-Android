d043dbb1e8023f7b0fccb7f0908c06d40d10d95e
Mike Wong <wong@ubique.ch>
2021-07-07 20:11:27 +0200

    Set correct status icon for certificate light based on verification state

--- wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/light/CertificateLightPagerFragment.kt
+++ wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/light/CertificateLightPagerFragment.kt
@@ -178,22 +178,25 @@ class CertificateLightPagerFragment : Fragment(R.layout.fragment_certificate_lig
 	private fun displaySuccessState() {
 		showLoadingIndicator(false)
 		setVerificationStateBubbleColor(R.color.blueish)
+		binding.certificatePageStatusIcon.setImageResource(R.drawable.ic_info_blue)
 		binding.certificatePageStatusInfo.setText(R.string.verifier_verify_success_certificate_light_info)
 	}
 
 	private fun displayInvalidState(state: VerificationState.INVALID) {
 		showLoadingIndicator(false)
 		setVerificationStateBubbleColor(R.color.greyish)
+		binding.certificatePageStatusIcon.setImageResource(R.drawable.ic_error_grey)
 		binding.certificatePageStatusInfo.text = state.getValidationStatusString(requireContext())
-
 	}
 
 	private fun displayErrorState(state: VerificationState.ERROR) {
 		showLoadingIndicator(false)
 		setVerificationStateBubbleColor(R.color.greyish)
 		if (state.isOfflineMode()) {
+			binding.certificatePageStatusIcon.setImageResource(R.drawable.ic_offline)
 			binding.certificatePageStatusInfo.text = getString(R.string.wallet_homescreen_offline).makeBold()
 		} else {
+			binding.certificatePageStatusIcon.setImageResource(R.drawable.ic_process_error_grey)
 			binding.certificatePageStatusInfo.text = getString(R.string.wallet_homescreen_network_error).makeBold()
 		}
 	}
