a65729cefcea5be6bf7c6b5990f9837f5f172ea9
Mike Wong <wong@ubique.ch>
2021-07-28 17:46:49 +0200

    Delete pdf file after sharing

---
+++ wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/pdf/export/PdfExportShareContract.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package ch.admin.bag.covidcertificate.wallet.pdf.export
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.result.contract.ActivityResultContract
+import ch.admin.bag.covidcertificate.wallet.R
+
+class PdfExportShareContract : ActivityResultContract<Uri, Uri>() {
+
+	private var uri: Uri? = null
+
+	override fun createIntent(context: Context, input: Uri?): Intent {
+		this.uri = input
+		return Intent(Intent.ACTION_SEND).apply {
+			type = "application/pdf"
+			putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.wallet_certificate))
+			putExtra(Intent.EXTRA_STREAM, input)
+		}
+	}
+
+	override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+		return uri
+	}
+}
