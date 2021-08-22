fda8d43f2d3b405e26505b0d749095f835614b4d
Thore Goebel <goebel@ubique.ch>
2021-08-22 17:58:33 +0200

    Show nice chooser to share PDF

--- wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/pdf/export/PdfExportShareContract.kt
+++ wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/pdf/export/PdfExportShareContract.kt
@@ -22,11 +22,12 @@ class PdfExportShareContract : ActivityResultContract<Uri, Uri>() {
 
 	override fun createIntent(context: Context, input: Uri?): Intent {
 		this.uri = input
-		return Intent(Intent.ACTION_SEND).apply {
+		val intent = Intent(Intent.ACTION_SEND).apply {
 			type = "application/pdf"
 			putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.wallet_certificate))
 			putExtra(Intent.EXTRA_STREAM, input)
 		}
+		return Intent.createChooser(intent, null)
 	}
 
 	override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
