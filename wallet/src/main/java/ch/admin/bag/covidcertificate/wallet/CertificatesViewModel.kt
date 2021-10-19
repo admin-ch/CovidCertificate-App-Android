678f75fc226b80b2fd83eadbd7c7ebea68d7f910
Mike Wong <wong@ubique.ch>
2021-10-19 16:36:00 +0200

    Add read-write lock to wallet data and add check before deleting transfer code on server

--- wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/CertificatesViewModel.kt
+++ wallet/src/main/java/ch/admin/bag/covidcertificate/wallet/CertificatesViewModel.kt
@@ -36,8 +36,12 @@ import ch.admin.bag.covidcertificate.wallet.transfercode.model.TransferCodeConve
 import ch.admin.bag.covidcertificate.wallet.transfercode.model.TransferCodeModel
 import ch.admin.bag.covidcertificate.wallet.transfercode.net.DeliveryRepository
 import ch.admin.bag.covidcertificate.wallet.transfercode.net.DeliverySpec
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.time.Instant
 import kotlin.collections.set
@@ -257,11 +261,14 @@ class CertificatesViewModel(application: Application) : AndroidViewModel(applica
 					val decryptedCertificates = deliveryRepository.download(transferCode.code, keyPair)
 
 					if (decryptedCertificates.isNotEmpty()) {
+						var didReplaceTransferCode = false
+
 						decryptedCertificates.forEachIndexed { index, convertedCertificate ->
 							val qrCodeData = convertedCertificate.qrCodeData
 							val pdfData = convertedCertificate.pdfData
+
 							if (index == 0) {
-								walletDataStorage.replaceTransferCodeWithCertificate(transferCode, qrCodeData, pdfData)
+								didReplaceTransferCode = walletDataStorage.replaceTransferCodeWithCertificate(transferCode, qrCodeData, pdfData)
 								val decodeState = CovidCertificateSdk.Wallet.decode(qrCodeData)
 								conversionState = if (decodeState is DecodeState.SUCCESS) {
 									TransferCodeConversionState.CONVERTED(decodeState.certificateHolder)
@@ -272,7 +279,11 @@ class CertificatesViewModel(application: Application) : AndroidViewModel(applica
 							} else {
 								walletDataStorage.saveWalletDataItem(WalletDataItem.CertificateWalletData(qrCodeData, pdfData))
 							}
+						}
 
+						// Delete the transfer code on the backend and the key pair only if the certificate was stored (either by the above replace method or from another thread)
+						val didStoreCertificate = walletDataStorage.containsCertificate(decryptedCertificates.first().qrCodeData)
+						if (didReplaceTransferCode || didStoreCertificate) {
 							try {
 								deliveryRepository.complete(transferCode.code, keyPair)
 							} catch (e: IOException) {
