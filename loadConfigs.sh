eb8ae7e3045ec62689ad62d3971b044b95c9d5b8
Mike Wong <wong@ubique.ch>
2022-01-21 13:41:10 +0100

    Fix prod output path

--- loadConfigs.sh
+++ loadConfigs.sh
@@ -7,13 +7,15 @@ timestamp=$(date +%s%N | cut -b1-13)
 
 for app in ${apps[@]}; do
   for environment in ${environments[@]}; do
-    output="${app}/src/${environment}/assets/faq/config.json"
     if [[ $environment = "prod" ]]; then
       subdomain="cc-a" # Take the prod config from the ABN environment
+      output="${app}/src/main/assets/faq/config.json"
     elif [ $environment = "abn" ]; then
       subdomain="cc-a"
+      output="${app}/src/${environment}/assets/faq/config.json"
     elif [ $environment = "dev" ]; then
       subdomain="cc-d"
+      output="${app}/src/${environment}/assets/faq/config.json"
     fi
 
     versionName=$(cat "${app}/build.gradle" | sed -n -e 's/^.*versionName\s*"\(.*\)"$/\1/p')
